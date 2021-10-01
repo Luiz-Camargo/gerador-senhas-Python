6801b1652fd8266b4cf48b30c4b825593bea783b
Luiz-Camargo <luiz.henriquecamargo@hotmail.com>
2021-10-01 14:05:53 -0300

    Primeiro commit

---
+++ gerador.py
@@ -0,0 +1,17 @@
+import random
+import PySimpleGUI as sg #FAZER TELAS
+
+# GERADOR DE SENHAS
+
+minusculo = ("abcdefghjklmnopqrstuvwxyz")
+maiusculo = ("ABCDEFGHJKLMNOPQRSTUVWXYZ")
+numeros = ("0123456789")
+simbolos = ("@#$%&*")
+
+varjuntas = minusculo + maiusculo + numeros + simbolos
+tamanho = input("Insira o tamanho da senha:")
+nmrsenhas = input("Informe o número de senhas que deve ser gerada:")
+
+for senha in range(int(nmrsenhas)):
+    senha = random.sample(varjuntas, int(tamanho))
+    print("".join(senha))
